0bbe3c85a9681a987f33d0594fc84a5c7bf68bcc
Robert Honz <exislow+github@gmail.com>
2024-11-08 17:41:39 +0100

    Reverting due to `tidalapi` bug.

--- tidal_dl_ng/download.py
+++ tidal_dl_ng/download.py
@@ -94,7 +94,7 @@ class Download:
 
         # Get urls for media.
         if isinstance(media, Track):
-            urls = media.get_stream().get_stream_manifest().get_urls()
+            urls = media.get_stream().get_stream_manifest().urls
             stream_manifest: StreamManifest = media.get_stream().get_stream_manifest()
         elif isinstance(media, Video):
             m3u8_variant: m3u8.M3U8 = m3u8.load(media.get_url())
