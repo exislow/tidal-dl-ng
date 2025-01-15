e0d5714da090d91d4e18b2e061117a6ed130934c
Ryoko7 <140979505+ryoko7@users.noreply.github.com>
2025-01-14 20:01:18 -0500

    try to add imports for the 4th time without pre-commit deleting it

---
+++ tidal_dl_ng/interface.py
@@ -0,0 +1,7 @@
+from tidal_dl_ng.config import Settings, Tidal
+from tidal_dl_ng.download import Download
+from tidal_dl_ng.helper.tidal import items_results_all
+from tidal_dl_ng.helper.wrapper import LoggerWrapped
+from tidal_dl_ng.model.cfg import Settings as ModelSettings
+from tidalapi import Album, Artist, Mix, Playlist, Track
+from tidalapi.session import SearchResults
